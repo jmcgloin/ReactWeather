9fba53d1f981d66344b439df30b6fb68eb5548f6
Jason <jason.mcgloin@gmail.com>
2017-02-16 23:18:04 -0500

    Update the About page

--- app/component/About.jsx
+++ app/component/About.jsx
@@ -3,8 +3,10 @@ var React = require('react');
 let About = (props) => {
 	return (
 		<div>
-			<h3>About!</h3>
-			<p>Welcome to the About page</p>
+			<h1 className='text-center'>About!</h1>
+			<p className='text-center'>
+				I built this app using "The Complete React Web Appp Developer Course" by Andrew Mead on Udemy as a guide to learn to use React.js.  Along with a few other tools like Foundation, Express, Webpack, and OpenWeatherMap.org, I have been able to build this app and learn to employ React.
+			</p>
 		</div>
 
 	);
