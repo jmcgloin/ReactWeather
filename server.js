135e99db2656851b3676db0fd3464c0c90bb250e
Jason <jason.mcgloin@gmail.com>
2017-02-16 21:20:21 -0500

    Updated for heroku

--- server.js
+++ server.js
@@ -3,12 +3,10 @@ var express = require('express');
 let app = express();
 const PORT = process.env.PORT || 3000;
 
-if(PORT !== 3000) {
-	app.use((req,res,next) => {
-		req.headers['x-forwarded-proto'] === 'http' ?
-			next() : res.redirect('http://' + req.hostname + req.url);
-	});
-}
+app.use((req,res,next) => {
+	req.headers['x-forwarded-proto'] === 'https' ?
+		res.redirect('http://' + req.hostname + req.url): next();
+});
 
 app.use(express.static('public'));
 app.listen(PORT, () => console.log(`Express server up on port ${PORT}`));
