545ab51c6808370621b2c589885afae581e67c3f
Jason <jason.mcgloin@gmail.com>
2017-02-16 21:33:39 -0500

    Updated About page

--- app/component/Examples.jsx
+++ app/component/Examples.jsx
@@ -2,7 +2,10 @@ var React = require('react');
 
 let Examples = (props) => {
 	return (
-		<h3>Examples!</h3>
+		<div>
+			<h3>Examples!</h3>
+			<p>Welcome to the Examples page</p>
+		</div>
 	);
 };
 
